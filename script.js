95f25f7975b80467e258b467a7ce0748e664798f
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2024-09-24 22:22:43 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+const techStack = document.getElementById('tech-stack');
+
+techStack.addEventListener('mouseover', () => {
+    techStack.style.animationPlayState = 'paused';
+});
+
+techStack.addEventListener('mouseout', () => {
+    techStack.style.animationPlayState = 'running';
+});
